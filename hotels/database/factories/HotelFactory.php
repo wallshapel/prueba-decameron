9b56cda56c059e13914cb1564c64c7240a2e63ce
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-04-22 16:43:43 -0500

    Factory and Faker for Hotel

---
+++ hotels/database/factories/HotelFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hotel>
+ */
+class HotelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'Hotel ' . $this->faker->city(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'nit' => $this->faker->unique()->numerify('########-#'),
+            'room_limit' => $this->faker->numberBetween(10, 50),
+        ];
+    }
+}
