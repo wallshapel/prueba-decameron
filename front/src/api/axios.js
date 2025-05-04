a3d373db7bad56a54070e3159e4ad61289832b9f
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-05-04 04:55:47 -0500

    Axios base url changed

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  // Local/Docker
+  // baseURL: 'http://localhost:8000/api/v1',
+  // Railway
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
