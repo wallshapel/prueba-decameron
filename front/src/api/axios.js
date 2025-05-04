ba1473bb4119eaefcab4a9afdaf66a3ceca053da
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-05-04 06:14:41 -0500

    Updating axios base url and package.json to Railway deploay

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -4,7 +4,7 @@ const api = axios.create({
   // Local/Docker
   // baseURL: 'http://localhost:8000/api/v1',
   // Railway
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1',
+  baseURL: 'https://laravel-production-9569.up.railway.app/api/v1',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
